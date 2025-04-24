88e0b6b736545dbd64a2ffcd0e2defa00a3fc33f
Laksh Sachdeva <lakshsachdeva01@example.com>
2025-04-24 18:58:38 +0530

    tests updated

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+export default [
+    {
+      files: ["**/*.js"],
+      rules: {
+        semi: ["error", "always"],
+        quotes: ["error", "double"],
+        "no-unused-vars": "warn",
+      },
+    },
+  ];
+  
